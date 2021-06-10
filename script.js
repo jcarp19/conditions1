46da3c113c0450335f992623f8429b469ac4cde9
jcarp19 <jim@carpenteronline.us>
2021-06-10 08:14:14 -0400

    first push

---
+++ script.js
@@ -0,0 +1,10 @@
+let actualTemp = 74;
+let desiredTemp = 74;
+
+if (actualTemp > desiredTemp) {
+    console.log("Run A/C")
+} else if (actualTemp < desiredTemp) {
+    console.log("Run Heat")
+} else {
+    console.log("Standby")
+};
