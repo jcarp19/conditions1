48e7261907dcf0d5e8ff7f21ace4c8c0995fb6bd
jcarp19 <jim@carpenteronline.us>
2021-06-10 12:47:29 -0400

    final push

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let actualTemp = 74;
+let actualTemp = 80;
 let desiredTemp = 74;
 
 if (actualTemp > desiredTemp) {
@@ -8,3 +8,23 @@ if (actualTemp > desiredTemp) {
 } else {
     console.log("Standby")
 };
+
+
+
+console.log("Extended Challenge")
+let tempCelsius = 100;
+let targetUnit = "F";
+
+const choice = prompt("Select a temperature unit to convert(C, F, or K)")
+switch (choice) {
+    case "C":
+        console.log("Your converted temperature is, " + tempCelsius + " (no conversion needed)");
+        break;
+    case "F":
+        console.log("Your converted temperature is, " + ((tempCelsius * (9/5)) + 32));
+        break;
+    case "K":
+        console.log("Your converted temperature is, " + (tempCelsius + 273.15));
+        break;
+        
+}
